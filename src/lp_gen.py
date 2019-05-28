a8122d5d2ad363d73999b32975044b7fcf4420c6
Jesse Sheehan <jesse@sheehan.nz>
2019-05-29 11:12:25 +1200

    Added more constraints. Report should be up to date with code.

--- src/lp_gen.py
+++ src/lp_gen.py
@@ -2,26 +2,44 @@ from lp_utils import perms, concat
 
 template = """\
 \\ COSC-364 Assignment 2, LP Output File
+
 MINIMIZE
-    r
+\tr
+
 SUBJECT TO
-    \\ DEMAND CONSTRAINTS
-    {}
-    \\ CAPACITY CONSTRAINTS FOR LINKS BETWEEN SOURCE AND TRANSIT NODES
-    {}
-    \\ CAPACITY CONSTRAINTS FOR LINKS BETWEEN TRANSIT AND DESTINATION NODES
-    {}
-    \\ TRANSIT NODE LOAD CONSTRAINTS
-    {}
-    \\ BINARY VARIABLE CONSTRAINTS (ONLY 2 ACTIVE TRANSIT NODES)
-    {}
+
+\t\\ DEMAND CONSTRAINTS
+\t{}
+
+\t\\ CAPACITY CONSTRAINTS FOR LINKS BETWEEN SOURCE AND TRANSIT NODES
+\t{}
+
+\t\\ CAPACITY CONSTRAINTS FOR LINKS BETWEEN TRANSIT AND DESTINATION NODES
+\t{}
+
+\t\\ OBJECTIVE FUNCTION LOAD CONSTRAINTS
+\t{}
+
+\t\\ TRANSIT NODE LOAD CONSTRAINTS
+\t{}
+
+\t\\ BINARY VARIABLE AND DECISION VARIABLE CONSTRAINTS
+\t{}
+
+\t\\ BINARY VARIABLE CONSTRAINTS (ONLY 2 ACTIVE TRANSIT NODES)
+\t{}
+
 BOUNDS
-    \\ NON-NEGATIVITY CONSTRAINTS
-    r >= 0
-    {}
+
+\t\\ NON-NEGATIVITY CONSTRAINTS
+\tr >= 0
+\t{}
+
 BIN
-    \\ BINARY VARIABLES
-    {}
+
+\t\\ BINARY VARIABLES
+\t{}
+
 END
 """
 
@@ -44,24 +62,30 @@ def get_source_transit_capacity_constraints(s, t, d):
     """ Returns a list of capacity constraints for the links between the source and transit nodes. """
     return \
         [' + '.join(["X_{0}{1}{2}".format(i, k, j) for j in d]) +
-            ' - C_{0}{1} <= 0'.format(i, k) for (i, k) in perms([s, t])]  # + \
-    # [' + '.join(["C_{0}{1}".format(i, j) for i in s]) +
-    # ' - r <= 0' for j in d]
-    # don't know about the above commented lines
+            ' - C_{0}{1} = 0'.format(i, k) for (i, k) in perms([s, t])]
 
 
 def get_transit_destination_capacity_constraints(s, t, d):
     """ Returns a list of capacity constraints for the links between the transit and destination nodes. """
     return \
         [' + '.join(["X_{0}{1}{2}".format(i, k, j) for i in s]) +
-            ' - D_{0}{1} <= 0'.format(k, j) for (k, j) in perms([t, d])]
+            ' - D_{0}{1} = 0'.format(k, j) for (k, j) in perms([t, d])]
 
 
 def get_transit_load_constraints(s, t, d):
     """ Returns the list of transit load constraints. """
     return [' + '.join(["X_{0}{1}{2}".format(i, k, j) for (i, j) in perms([s, d])]) +
-            ' - r <= 0' for k in t]  # maybe change this line for the one below?
-    # ' - L_{0} <= 0'.format(k) for k in t]
+            ' - l_{0} = 0'.format(k) for k in t]
+
+def get_objective_function_load_constraints(s, t, d):
+    """ Returns the list of objective function load constraints. """
+    return []
+    #return [' + '.join(["X_{0}{1}{2}".format(i, k, j) for (i, j) in perms([s, d])]) +
+    #        ' - r <= 0' for k in t]
+
+def get_binary_and_decision_variable_constraints(s, t, d):
+    """ Returns the binary and decision variable constraints. """
+    return []
 
 
 def get_binary_constraints(s, t, d):
@@ -91,8 +115,10 @@ def generate_lp_file(x, y, z):
         get_transit_destination_capacity_constraints(s, t, d))
     non_negativity_constraints = '\n\t'.join(get_non_negativity_constraints(
         s, t, d))
+    objective_function_load_constraints = '\n\t'.join(get_objective_function_load_constraints(s, t, d))
     transit_load_constraints = '\n\t'.join(
         get_transit_load_constraints(s, t, d))
+    binary_and_decision_constraints = '\n\t'.join(get_binary_and_decision_variable_constraints(s, t, d))
     binary_variable_constraints = '\n\t'.join(get_binary_constraints(s, t, d))
     binary_variables = '\n\t'.join(get_binary_variables(s, t, d))
 
@@ -100,7 +126,9 @@ def generate_lp_file(x, y, z):
         demand_constraints,
         source_transit_capacity_constraints,
         transit_destination_capacity_constraints,
+        objective_function_load_constraints,
         transit_load_constraints,
+        binary_and_decision_constraints,
         binary_variable_constraints,
         non_negativity_constraints,
         binary_variables)
