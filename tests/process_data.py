162df134a8c95168e012f6244170030e962d8b5c
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-29 21:08:20 +1200

    Added configuration via command line args

--- tests/process_data.py
+++ tests/process_data.py
@@ -1,18 +1,22 @@
 import csv
+import sys
+import os.path
+
 
 def csvWrite(data):
-    with open('out.csv', 'a') as csvFile:
+    with open(sys.argv[2], 'a') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow(data)
-        
+
     return
 
+
 def openFile(Y):
-    with open('{0}.txt'.format(Y), 'r') as in_file:
+    with open(os.path.join(sys.argv[1], '{0}.txt'.format(Y)), 'r') as in_file:
         stripped = [line.strip() for line in in_file.readlines()]
         lines = [line for line in stripped if line]
         data = []
-        #Y
+        # Y
         data.append(Y)
         # elapsed time
         data.append(max(parseFile("elapsed", lines)))
@@ -23,31 +27,37 @@ def openFile(Y):
         # smallest_transit_node_load
         data.append(min(parseFile("l_", lines)))
         # largest_transit_node_load
-        data.append(max(parseFile("l_", lines)))     
+        data.append(max(parseFile("l_", lines)))
         # highest cap c network
         data.append(max(parseFile("c_", lines)))
         # highest cap d network
-        data.append(max(parseFile("d_", lines)))         
+        data.append(max(parseFile("d_", lines)))
         print(data)
         csvWrite(data)
-        
-        return
-        
 
-'''Returns a list of all values that start with the given string'''        
+        return
+
+
+'''Returns a list of all values that start with the given string'''
+
+
 def parseFile(string, lines):
     values = []
     for line in lines:
         if line.startswith(string):
             values.append(line.split()[1])
-            
+
     return values
 
 
-openFile(3)
-openFile(4)
-openFile(5)
-openFile(6)
-openFile(7)
-openFile(8)
-        
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: {0} <input directory> <csv file>".format(sys.argv[0]))
+        exit(-1)
+
+    openFile(3)
+    openFile(4)
+    openFile(5)
+    openFile(6)
+    openFile(7)
+    openFile(8)
