80937c2da673e05462fae687f9cb1ce255b63252
Jesse Sheehan <jps111@uclive.ac.nz>
2019-05-29 22:19:14 +1200

    Changed the location of the cplex_data.csv file

--- output.ps1
+++ output.ps1
@@ -1,9 +1,9 @@
 For ($i=3; $i -le 8; $i++) {
     python src 9 $i 9 lp_files
-    $perf = Measure-Command -Expression {$data = cplex -c ("read problem_9_" + $i + "_9.lp") "optimize" "display solution variables -"}
+    $perf = Measure-Command -Expression {$data = cplex -c ("read lp_files/problem_9_" + $i + "_9.lp") "optimize" "display solution variables -"}
     $ms = $perf.TotalMilliseconds
     [System.IO.File]::WriteAllLines("cplex_logs/$i.txt", $data + "`nelapsed_time: $ms ms")
 }
 
-python src/lp_csv.py cplex_logs cplex_data.csv
-python src/lp_graph.py cplex_data.csv graphs
+python src/lp_csv.py cplex_logs lp_files/cplex_data.csv
+python src/lp_graph.py lp_files/cplex_data.csv graphs
--- output.sh
+++ output.sh
@@ -3,7 +3,7 @@ for y in 3 4 5 6 7 8
 do
    python3 src 9 $y 9 lp_files
    start=$(date +%s%N)
-   cplex -c "read problem_9_${y}_9.lp" "optimize" "display solution variables -" > cplex_logs/$y.txt
+   cplex -c "read lp_files/problem_9_${y}_9.lp" "optimize" "display solution variables -" > cplex_logs/$y.txt
    end=$(date +%s%N)
    duration=$(expr $end - $start)
    duration=$(expr $duration / 1000000)
